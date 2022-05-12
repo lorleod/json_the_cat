1bb5070fb18044738873c5d2404c7305dfb149c0
lorleod <taylor.mcleod@gmail.com>
2022-05-12 23:49:30 +0000

    finished breedfetcher

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,2 +1,16 @@
-const request = require('request');
+const request = require("request");
+const args = process.argv.slice(2);
+const URI = `https://api.thecatapi.com/v1/breeds/search?q=${args[0]}`;
 
+request(URI, (error, response, body) => {
+  if (error) {
+    console.log("Error: ", error);
+  } else {
+    const data = JSON.parse(body);
+    if (!data[0]) {
+      console.log("No breed with that name found");
+    } else {
+      console.log(``, data[0].description);
+    }
+  }
+});
